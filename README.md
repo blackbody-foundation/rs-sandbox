c341b0dfb6ad92977f00a0d1b74eb51b93117b94
just-do-halee <just.do.halee@gmail.com>
2022-08-11 11:46:20 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rust sandbox
+
+- udp
---
+++ udp/src/main.rs
@@ -0,0 +1,51 @@
+use std::{error, io, net, str};
+
+use console;
+use crossbeam;
+// thread 1 : receiving BB,
+//      A: if BB is mine, connecting Group (TCP)
+//      B: else,
+//          if BB needs last signing: signing BB, returning BB to creator (UDP)
+//          else, signing BB, spreading BB (UDP)
+// phase start : creating BB, spreading BB
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let client_addr = "127.0.0.1:5451";
+    let server_addr = "127.0.0.1:8888";
+
+    let term = console::Term::stdout();
+    term.write_line("Please 'Enter' to send a message.")?;
+
+    crossbeam::thread::scope(|s| {
+        // server role
+        s.spawn(|_| -> io::Result<()> {
+            loop {
+                let udp_socket = net::UdpSocket::bind(server_addr)?;
+
+                let mut buf = [0; 100];
+                let (amount, _) = udp_socket.recv_from(&mut buf)?;
+                let s = str::from_utf8(&buf[..amount]).unwrap();
+                term.write_line(&format!("Received!: {s}\r"))?;
+            }
+        });
+
+        // client role
+        s.spawn(|_| -> io::Result<()> {
+            loop {
+                let a = term.read_key()?;
+                if a == console::Key::Enter {
+                    let message = "Test Message";
+
+                    term.write_line(&format!("Send! {message}"))?;
+
+                    let udp_socket = net::UdpSocket::bind(client_addr)?;
+
+                    udp_socket.send_to(message.as_bytes(), server_addr)?;
+                }
+            }
+        });
+    })
+    .unwrap();
+
+    Ok(())
+}
